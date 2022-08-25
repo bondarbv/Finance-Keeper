c7c339c9156fea26f8448bc59c0ea32fc4ac044f
bondarbv <92814470+bondarbv@users.noreply.github.com>
2022-08-25 11:58:39 +0300

    Added transactionButton on MainViewController

--- Finance Keeper/Main/MainViewController.swift
+++ Finance Keeper/Main/MainViewController.swift
@@ -15,9 +15,7 @@ final class MainViewController: UIViewController {
     
     //MARK: - Balance UI
     private lazy var balanceStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [currentBalanceLabel,
-                                                       currentBalanceValueLabel,
-                                                       incomeAndExpenseStackView])
+        let stackView = UIStackView(arrangedSubviews: [currentBalanceLabel, currentBalanceValueLabel])
         stackView.axis = .vertical
         stackView.spacing = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -28,6 +26,7 @@ final class MainViewController: UIViewController {
         let stackView = UIStackView(arrangedSubviews: [incomeStackView, expenseStackView])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
@@ -90,6 +89,15 @@ final class MainViewController: UIViewController {
         return label
     }()
     
+    //MARK: - Transaction button
+    private lazy var transactionButton: GradientButton = {
+        let button = GradientButton()
+        button.gradientLayer.cornerRadius = 50
+        button.addTarget(self, action: #selector(addTransaction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     //MARK: - TransactionsTableView
     private lazy var transactionsTableVew: UITableView = {
        let tableView = UITableView()
@@ -119,24 +127,34 @@ final class MainViewController: UIViewController {
         let settingsImage = UIImage(named: "settings")
         let filterImage = UIImage(named: "filter")
 
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: settingsImage, style: .plain, target: self, action: #selector(addTransaction))
-        navigationItem.rightBarButtonItem?.tintColor = UIColor.tabBarSelectedItemColor
-        
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: filterImage, style: .plain, target: self, action: #selector(addTransaction))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: settingsImage, style: .plain, target: self, action: #selector(addTransaction))
         navigationItem.leftBarButtonItem?.tintColor = UIColor.tabBarSelectedItemColor
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: filterImage, style: .plain, target: self, action: #selector(addTransaction))
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.tabBarSelectedItemColor
     }
     
     //MARK: - Layout
     private func layout() {
         view.addSubview(balanceStackView)
+        view.addSubview(transactionButton)
+        view.addSubview(incomeAndExpenseStackView)
         view.addSubview(transactionsTableVew)
         
         NSLayoutConstraint.activate([
             balanceStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             balanceStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            balanceStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            transactionsTableVew.topAnchor.constraint(equalTo: balanceStackView.bottomAnchor, constant: 10),
+            transactionButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            transactionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            transactionButton.widthAnchor.constraint(equalToConstant: 100),
+            transactionButton.heightAnchor.constraint(equalToConstant: 100),
+            
+            incomeAndExpenseStackView.topAnchor.constraint(equalTo: balanceStackView.bottomAnchor, constant: 20),
+            incomeAndExpenseStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            incomeAndExpenseStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            transactionsTableVew.topAnchor.constraint(equalTo: incomeAndExpenseStackView.bottomAnchor, constant: 10),
             transactionsTableVew.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             transactionsTableVew.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             transactionsTableVew.trailingAnchor.constraint(equalTo: view.trailingAnchor)
